bf0a203981b08e3e7dc941805238350eadd746f5
JustAnElad <elad12.es@gmail.com>
2023-02-14 01:05:53 +0200

    Add project files.

---
+++ MP3/Play.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+
+namespace MP3
+{
+    public static class Play
+    {
+        //Creates a SoundPlayer field
+        public static SoundPlayer soundPlayer = new SoundPlayer();
+
+        //Creates a songIndex variable
+        public static int songIndex;
+
+        //Song thread execution
+        public static async Task PlaySong()
+        {
+            Console.WriteLine("task started");
+            soundPlayer.PlaySync();
+        }
+
+
+        //Creates a song looping
+        public static async Task<int> SongLoop(string[] songs)
+        {
+           
+            //prints songs length
+            Console.WriteLine("songs amount = " + songs.Length);
+
+            for (songIndex = 0; songIndex < songs.Length; songIndex++)
+            {
+                soundPlayer.SoundLocation = songs[songIndex];
+                //prints the index of the songs array for debugging
+                Console.WriteLine("Song number: " + songIndex);
+
+                await PlaySong();
+            }
+
+            return songIndex;
+        }
+        //Menu function
+        public static async void Menu(string[] songsList)
+        {
+            while (true)
+            {
+
+                switch (Console.ReadLine())
+                {
+                    case "/play":
+
+                        await Play.SongLoop(songsList);
+                        break;
+
+                    case "/skip":
+
+                        Play.SkipSong(songsList);
+                        break;
+
+                    case "/stop":
+                        Play.StopSong();
+                        break;
+
+                    default:
+                        Console.WriteLine("No songs to play.");
+                        break;
+                }
+            }
+        }
+        public static void StopSong()
+        { 
+            soundPlayer.Stop();
+        }
+        public static void SkipSong(string[] songs)
+        {
+            /*int songCounter;
+
+            for(songCounter = 0; songCounter < songs.Length; songCounter++)
+            {
+                
+            }
+            */
+
+            if (songIndex++ > songs.Length)
+            {
+               soundPlayer.SoundLocation = songs[0];
+               songIndex = 0;
+                Console.WriteLine("Reset");
+            }
+            else if (songIndex++ == songs.Length)
+            {
+                songIndex = 0;
+                soundPlayer.SoundLocation = songs[0];
+                
+            }
+            else
+            {
+                soundPlayer.SoundLocation = songs[songIndex++];
+                Console.WriteLine("Sound location = " + songIndex);
+            }
+            
+            soundPlayer.Play();
+
+            /*soundPlayer.Stop();
+            soundPlayer.SoundLocation = songs[songIndex++];
+            soundPlayer.Play();
+            */
+        }
+    }
+}
---
+++ MP3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Media;
+using System.IO;
+
+
+
+namespace MP3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string folder = desktop + "/nice_ass_songs/";
+
+            //create directory
+            if (!Directory.Exists(folder))
+            {
+                DirectoryInfo directoryInfo = Directory.CreateDirectory(folder);
+                directoryInfo.Attributes = FileAttributes.Directory;
+            }
+
+            FileInfo[] files = new DirectoryInfo(folder).GetFiles();
+
+            // Check if the array is empty
+            if (files.Length == 0)
+            {
+                Console.WriteLine("The folder is empty.");
+
+            }
+            
+            Console.Clear();
+            //creates songs' array
+            string[] songsList = new string[files.Length];
+            //fills the array with the songs' name
+            for( int i = 0; i < songsList.Length; i++)
+            {
+                songsList[i] = folder + files[i].Name;
+                Console.WriteLine(songsList[i]);
+            }
+             Play.Menu(songsList);
+            
+        }
+    }
+}
